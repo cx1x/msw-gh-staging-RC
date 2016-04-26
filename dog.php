ca583a8d1a7a7703d9b2e1390a305eb47662b077
cx1x <crealubit@ph.esl-asia.com>
2016-04-26 17:29:52 +0800

    test commit

--- dog.php
+++ dog.php
@@ -58,48 +58,19 @@ $_track_name = array(
 
 <html lang="en">
 
-	<head>
-	
-		<meta charset="utf-8">
-		
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		
-		<title>MSW Greyhounds</title>
-
-		<!-- Bootstrap -->
-		<link href="<?=$folder;?>/css/bootstrap.min.css" rel="stylesheet">
-
-		<link rel="stylesheet" href="<?=$folder;?>/css/reset.css"> <!-- CSS reset -->
-
-		<link rel="stylesheet" href="<?=$folder;?>/css/style-anim.css"> <!-- Resource style -->
-
-		<script src="<?=$folder;?>/js/modernizr.js"></script> <!-- Modernizr -->
-
-		<link href="<?=$folder;?>/css/font-awesome.min.css" rel="stylesheet">
-
-		<link href="<?=$folder;?>/css/style.css" rel="stylesheet">
-
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	
-	</head>
+	<?php
+	include_once('header.php');
+	?>
 
 	<body>
-	
-		<div class="load-bar">
-			  <div class="bar"></div>
-			  <div class="bar"></div>
-			  <div class="bar"></div>
-		</div>
 
 		<main>
+
+			<div class="load-bar">
+			  <div class="bar"></div>
+			  <div class="bar"></div>
+			  <div class="bar"></div>
+			</div>
 
 			<div class="cd-main-content cd-dog">
 
@@ -381,13 +352,9 @@ $_track_name = array(
 
 		</main>
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="<?=$folder;?>/js/bootstrap.min.js"></script>
-
-		<script src="<?=$folder;?>/js/main.js"></script> <!-- Resource jQuery -->
+		<?php
+		include_once('footer.php');
+		?>
 
 		<script>
 		
